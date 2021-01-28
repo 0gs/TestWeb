2cf4c4299c52edd9b4a61f0389cd0f971d0f0cee
G <G@G>
2021-01-28 16:44:32 +0200

    12. with new animation

---
+++ js/main.js
@@ -0,0 +1,19 @@
+anime ({
+    targets: 'div.box',
+    translateY: [
+        {value: 400, duration: 1600},
+        {value:0, duration: 1600}  
+    ],
+    rotate: {
+    value: '1turn',
+    },
+    borderRadius: 50,
+    direction: 'alternate',
+    easing: 'easeInOutQuad',
+    delay: function() { return anime.random(0, 1000); },
+    // autoplay: false,
+    loop: true,
+    elasticity: 200 
+   
+}); 
+playPause.play();
