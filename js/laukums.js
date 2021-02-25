d2d04fae90461d3506e04531adad840407863cd9
G <G@G>
2021-02-26 00:30:41 +0200

    laukums update

---
+++ js/laukums.js
@@ -0,0 +1,26 @@
+function create(){
+    let x = document.getElementById("x").value;
+    console.log(x);
+    
+    let table = document.getElementById("laukums");
+    table.innerHTML = '';
+    
+    for(let i =0; i< x; i++){
+        let row = table.insertRow(i);
+        
+        for(let j =0; j< x; j++){
+            let button = document.createElement('BUTTON');
+            button.classList.add("grid")
+            
+            var text = document.createTextNode(x*i+j+1);
+            button.appendChild(text);
+            
+            let cell = row.insertCell(j);
+            cell.appendChild(button);
+        }   
+    }
+}
+    let doThis = document.getElementById("generate");
+    doThis.addEventListener("click", create);
+
+  
