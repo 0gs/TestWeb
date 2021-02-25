0abe3eccf5a55fee417c5c0c9b9751f31b8c6573
G <G@G>
2021-02-26 00:37:53 +0200

    laukums update 2

--- js/laukums.js
+++ js/laukums.js
@@ -17,6 +17,10 @@ function create(){
             
             let cell = row.insertCell(j);
             cell.appendChild(button);
+            
+            button.onclick=()=>{
+                button.parentNode.parentNode.parentNode.remove();
+            };
         }   
     }
 }
