d4a6896b5aa05a352d39b2a5f539dc4762015cfc
G <G@G>
2021-03-12 11:08:56 +0200

    stage 2 of memory game

--- js/laukums.js
+++ js/laukums.js
@@ -7,14 +7,7 @@ let LogoIMG = ["images/youtube.png", "images/word.png", "images/winrar.png", "im
                 "images/mozilla.png", "images/instagram.png", "images/illustrator.png", "images/flstudio.png", 
                 "images/facebook.png", "images/excel.png", "images/edge.png", "images/duolingo.png", 
                 "images/dropbox.png", "images/discord.png", "images/chrome.png", 
-                 "images/blender.png", "images/audacity.png", "images/apple.png"];         
-             
-//let LogoIMG = ["youtube", "word", "winrar", "windows", "whatsapp", "unity", "twitter",
-//                "twitch", "steam",  "spotify", "speedtest", "soundcloud", "snapchat",
-//                "skype", "shazam", "reddit", "powerpoint", "pinterest", "photoshop",
-//                "outlook", "netbeans", "mozilla", "instagram", "illustrator", "flstudio", 
-//                "facebook", "excel", "edge", "duolingo", "dropbox", "discord", "chrome", 
-//                 "blender", "audacity", "apple"];
+                 "images/blender.png", "images/audacity.png", "images/apple.png"];              
 
 let LogoName = ["YouTube", "Word", "WinRaR", "Windows", "Whatsapp", "Unity", "Twitter",
                 "Twitch", "Steam",  "Spotify", "Speedtest", "Soundcloud", "Snapchat",
@@ -24,6 +17,9 @@ let LogoName = ["YouTube", "Word", "WinRaR", "Windows", "Whatsapp", "Unity", "Tw
                  "Blender", "Audacity", "Apple"];
 
 var mix = [];
+var buttonItems = [];
+var buttonStack;
+var counter = 0;
 
 function create(){
     let x = document.getElementById("x").value; //grid size
@@ -34,65 +30,77 @@ function create(){
     
     mixing();
     
-    for(let i =0; i< x; i++){
+    for (let i = 0; i < x; i++) {
         let row = table.insertRow(i);
-        for(let j =0; j< x; j++){
+        for (let j = 0; j < x; j++) {
             let button = document.createElement('BUTTON');
             button.classList.add("grid")
-            
-            if(mix[x*i+j].charAt(0)=='i'){
-                let zaza = "url("+"'"+mix[x*i+j]+"'"+")center no-repeat";
-                button.style.background = zaza ;
-            }else button.innerHTML = mix[x*i+j];
-//            button.style.background = "url(mix[0]) center no-repeat ";
-            var text = document.createTextNode(x*i+j+1);
+
+            if (mix[x * i + j].charAt(0) == 'i') {
+                let zaza = "url(" + "'" + mix[x * i + j] + "'" + ")center no-repeat";
+                buttonItems[x * i + j] = zaza;
+//                button.style.background = zaza ;
+//                button.style.backgroundSize = 'contain';
+            } else {
+                buttonItems[x * i + j] = mix[x * i + j];
+//                button.innerHTML = mix[x*i+j];
+            }
+            ;
+            var text = document.createTextNode(x * i + j + 1);
 //            button.appendChild(text);
 
             let cell = row.insertCell(j);
             cell.appendChild(button);
-            
-//            button.onclick=()=>{
-//                button.parentNode.parentNode.parentNode.remove();
-//            };
-        }   
+
+            var first;
+            var second;
+            var klikk = 0;
+
+            button.onclick = () => {
+
+                if (counter == 2) {
+                    button.style.background = "";
+                    button.innerHTML = "";
+                    button.style.backgroundColor = 'yellow';
+                    counter = 0;
+
+                } else {
+                    if (mix[x * i + j].charAt(0) == 'i') {
+                        button.style.background = buttonItems[x * i + j];
+                        button.style.backgroundSize = 'contain';
+                        first = button.id;
+                        console.log(first);
+                    } else {
+                        button.innerHTML = buttonItems[x * i + j];
+                        second = button.id;
+                    }
+                }
+                counter++;
+            }
+        }
     }
 }
+
 
 let doThis = document.getElementById("generate");
 doThis.addEventListener("click", create);
 
 
-function mixing(){
+function mixing() {
     mix = [];
-    for(let i=0; i<(lalala*lalala*0.5);i++){
+    for (let i = 0; i < (lalala * lalala * 0.5); i++) {
+//        let lol = Math.floor(Math.random() * Math.floor(30));
         mix.push(LogoIMG[i]);
         mix.push(LogoName[i]);
-        console.log(mix);
+//        console.log(mix);
     }
-    
+
     console.log(mix.length);
-    for(let i=0;i<mix.length;i++){
+    for (let i = 0; i < mix.length; i++) {
         var rand = Math.floor(Math.random() * mix.length);
         var tmp = mix[rand];
         mix[rand] = mix[i];
-        mix[i]=tmp;
+        mix[i] = tmp;
     }
-    console.log(mix);
-}
-
-
-var klik = false;
-
-function mainaBildi(){
-    if (klik)document.getElementById("poga").src="maize.png";
-    else document.getElementById("poga").src="musa.png";
-    klik=!klik;
-}
-function maina(){
-    let stils1, stils2;
-    let tt=document.getElementById("pogab");
-    stils1=tt.style.visibility;
-    if (stils1=="visible")stils2="hidden"; else stils2="visible";
-    tt.style.visibility=stils2;
-    setTimeout(function(){tt.style.visibility=stils1;},2000);
+//    console.log(mix);
 }
