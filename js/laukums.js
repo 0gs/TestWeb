be47d34b6d58bf1c13c71db523f2ef8b8388d13c
G <G@G>
2021-03-26 00:48:12 +0200

    a game is born...

--- js/laukums.js
+++ js/laukums.js
@@ -16,91 +16,155 @@ let LogoName = ["YouTube", "Word", "WinRaR", "Windows", "Whatsapp", "Unity", "Tw
     "Facebook", "Excel", "Edge", "Duolingo", "Dropbox", "Discord", "Chrome",
     "Blender", "Audacity", "Apple"];
 
-var mix = [];
+var mixArray = [];
 var buttonItems = [];
-var buttonStack;
-var counter = 0;
+var clickCheck = false;
+var idCheck;
+var newArray = [];
+var backCheck = 0;
+var startTime;
+var clickedFirst;
+var x;
+var numX;
 
 function create() {
-    let x = document.getElementById("x").value; //grid size
+    newArray = [];
+    clickedFirst = false;
+    var x = document.getElementById("x").value; //grid size
     let table = document.getElementById("laukums"); //table
     table.innerHTML = ''; //old table clear
-
-    lalala = x;
-
-    mixing();
+    numX = x;
+    mixingItems();
 
     for (let i = 0; i < x; i++) {
         let row = table.insertRow(i);
         for (let j = 0; j < x; j++) {
             let button = document.createElement('BUTTON');
+            button.style.background = "";
+            button.style.background = 'url(images/cover.jpg)center no-repeat';
+            button.style.backgroundSize = 'contain';
+//            let imgCover = document.createElement("img");
+//            imgCover.setAttribute("src", "images/cover.jpg");
+//            imgCover.setAttribute("class", "imgCover");
+//            button.appendChild(imgCover);
             button.classList.add("grid")
+            button.setAttribute("id", x * i + j);
 
-            if (mix[x * i + j].charAt(0) == 'i') {
-                let zaza = "url(" + "'" + mix[x * i + j] + "'" + ")center no-repeat";
-                buttonItems[x * i + j] = zaza;
-//                button.style.background = zaza ;
-//                button.style.backgroundSize = 'contain';
+            if (mixArray[x * i + j].charAt(0) == 'i') {
+                let logoForButton = "url(" + "'" + mixArray[x * i + j] + "'" + ")center no-repeat";
+                buttonItems[x * i + j] = logoForButton;
             } else {
-                buttonItems[x * i + j] = mix[x * i + j];
-//                button.innerHTML = mix[x*i+j];
+                buttonItems[x * i + j] = mixArray[x * i + j];
             }
-            ;
-            var text = document.createTextNode(x * i + j + 1);
-//            button.appendChild(text);
 
+//            var text = document.createTextNode(x * i + j + 1);
             let cell = row.insertCell(j);
             cell.appendChild(button);
 
-            var first;
-            var second;
-            var klikk = 0;
-
-            button.onclick = () => {
-
-                if (counter == 2) {
-                    button.style.background = "";
-                    button.innerHTML = "";
-                    button.style.backgroundColor = 'white';
-                    counter = 0;
-
-                } else {
-                    if (mix[x * i + j].charAt(0) == 'i') {
-                        button.style.background = buttonItems[x * i + j];
-                        button.style.backgroundSize = 'contain';
-                        first = button.id;
-                        console.log(first);
-                    } else {
-                        button.innerHTML = buttonItems[x * i + j];
-                        second = button.id;
-                    }
+            button.onclick = function () {
+                if (!clickedFirst) {
+                    clickedFirst = true;
+                    startTime = new Date().getTime();
                 }
-                counter++;
+                if (newArray.indexOf(button.id) == -1) {
+                    switchIDs(button.id);
+                }
             }
         }
     }
 }
 
-
 let doThis = document.getElementById("generate");
 doThis.addEventListener("click", create);
 
-
-function mixing() {
-    mix = [];
-    for (let i = 0; i < (lalala * lalala * 0.5); i++) {
+function mixingItems() {
+    mixArray = [];
+    for (let i = 0; i < (numX * numX * 0.5); i++) {
 //        let lol = Math.floor(Math.random() * Math.floor(30));
-        mix.push(LogoIMG[i]);
-        mix.push(LogoName[i]);
-//        console.log(mix);
+        mixArray.push(LogoIMG[i]);
+        mixArray.push(LogoName[i]);
     }
+    for (let i = 0; i < mixArray.length; i++) {
+        var rand = Math.floor(Math.random() * mixArray.length);
+        var tmp = mixArray[rand];
+        mixArray[rand] = mixArray[i];
+        mixArray[i] = tmp;
+    }
+}
 
-    console.log(mix.length);
-    for (let i = 0; i < mix.length; i++) {
-        var rand = Math.floor(Math.random() * mix.length);
-        var tmp = mix[rand];
-        mix[rand] = mix[i];
-        mix[i] = tmp;
+function switchImages(w) {
+    if (clickCheck) {
+        document.getElementById(w).style.background = "";
+        document.getElementById(w).innerHTML = "";
+        document.getElementById(w).style.backgroundColor = 'white';
+        document.getElementById(w).style.background = 'url(images/cover.jpg)center no-repeat';
+        document.getElementById(w).style.backgroundSize = 'contain';
+    } else {
+        document.getElementById(w).innerHTML = "";
+        if (mixArray[w].charAt(0) == 'i') {
+            document.getElementById(w).style.background = "";
+            document.getElementById(w).innerHTML = "";
+            document.getElementById(w).style.backgroundColor = 'white';
+            document.getElementById(w).style.background = buttonItems[w];
+            document.getElementById(w).style.backgroundSize = 'contain';
+        } else {
+            document.getElementById(w).style.background = "";
+            document.getElementById(w).innerHTML = "";
+            document.getElementById(w).style.backgroundColor = 'white';
+            document.getElementById(w).innerHTML = buttonItems[w];
+        }
     }
-//    console.log(mix);
+}
+
+function switchIDs(q) {
+    if (backCheck == 1)
+        return;
+    if (!clickCheck) {
+        idCheck = q;
+        switchImages(q);
+        clickCheck = true;
+    } else {
+        if (q == idCheck) {
+            switchImages(q);
+            clickCheck = false;
+        } else {
+            clickCheck = false;
+            switchImages(q);
+            let poga1 = mixArray[idCheck];
+            let poga2 = mixArray[q];
+            if (LogoName.indexOf(poga1) == LogoIMG.indexOf(poga2) && LogoIMG.indexOf(poga1) == LogoName.indexOf(poga2)) {
+                newArray.push(idCheck);
+                newArray.push(q);
+                victory();
+                idCheck = -1;
+            } else {
+                clickCheck = true;
+                backCheck = 1;
+                setTimeout(function () {
+                    switchImages(q);
+                    switchImages(idCheck);
+                    clickCheck = false;
+                    backCheck = 0;
+                }, 500);
+            }
+        }
+    }
+}
+
+function victory() {
+    if (newArray.length == numX * numX) {
+        var endTime = new Date().getTime();
+        var timeSec = (endTime - startTime) / 1000;
+        if (timeSec > 60) {
+            let timeMin = timeSec / 60 - (timeSec / 60) % 1;
+            timeSec = Math.round((timeSec - 60 * timeMin) * 1000) / 1000;
+            let endResultTime1 = document.getElementById("winText");
+            endResultTime1.innerHTML = "Apsveicu, tu pabeidzi spēli! Tavs laiks ir " + timeMin + " un " + timeSec + " sekundes.";
+            $("#ex1").modal("show");
+        } else {
+            let endResultTime2 = document.getElementById("winText");
+            endResultTime2.innerHTML = "Apsveicu, tu pabeidzi spēli! Tavs laiks ir " + timeSec + " sekundes.";
+            $("#ex1").modal("show");
+        }
+    }
 }
